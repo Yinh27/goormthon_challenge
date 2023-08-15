69bd241550eb9f92779dac9cd857d9f8c92be864
윤인효(정보보호 1회차) <air12735@naver.com>
2023-08-15 16:36:50 +0000

    week1 test commit

---
+++ Week_1/day_1.py
@@ -0,0 +1,2 @@
+W, R = map(int, input().split())
+print(int(W * (1 + R/30)))
---
+++ Week_1/day_2.py
@@ -0,0 +1,10 @@
+N = int(input())
+T, M = map(int, input().split())
+
+for i in range(N):
+	M += int(input())
+
+T = (T + M // 60) % 24
+M %= 60
+
+print(f"{T} {M}")
